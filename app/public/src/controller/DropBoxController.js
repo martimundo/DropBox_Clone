fa6c180f79018bd055a56f90f6d270f3a85ab810
Marcos Artimundo <51058488+martimundo@users.noreply.github.com>
2023-07-11 09:49:24 -0300

    Iniciando projeto DropBox Clone

---
+++ app/public/src/controller/DropBoxController.js
@@ -0,0 +1,68 @@
+class DropBoxController {
+
+
+    constructor() {
+
+        this.btnSendFileEl = document.querySelector("#btn-send-file");
+        this.inputFilesEl = document.querySelector("#files");
+        this.snackModalEl = document.querySelector("#react-snackbar-root");
+        this.initEvents();
+    }
+
+    initEvents() {
+
+        this.btnSendFileEl.addEventListener('click', event => {
+            this.inputFilesEl.click();
+        });
+
+        this.inputFilesEl.addEventListener('change', event => {
+
+            console.log(event.target.files);
+
+            this.uploadTask(event.target.files);
+
+            this.snackModalEl.style.display = 'block';
+        });
+
+
+    }
+
+    uploadTask(files) {
+
+        let promisses = [];
+
+        [...files].forEach(files => {
+
+            promisses.push(new Promise((resolve, reject) => {
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+
+                    try {
+                        resolve(JSON.parse(ajax.responseText));
+                    } catch (e) {
+                        reject(e);
+                    }
+                };
+
+                ajax.onerror = event => {
+
+                    reject(event);
+                }
+                let formData = new FormData();
+
+                formData.append('input-file', files);
+
+                ajax.send(formData);
+
+            }));
+            
+        });
+
+        return Promise.all(promisses);
+
+    }
+}
