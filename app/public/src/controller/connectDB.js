f71b4e47635426a92fe24377c68b6684dfef2b46
Marcos Artimundo <51058488+martimundo@users.noreply.github.com>
2023-07-13 09:49:43 -0300

    Corrigida a conexão com o firebase

---
+++ app/public/src/controller/connectDB.js
@@ -0,0 +1,10 @@
+
+ const firebaseConfig = {
+     apiKey: "AIzaSyAupJy2fxcidP6SguF4ZiVbWcj7zb5mw30",
+     authDomain: "dropbox-clone-13ef0.firebaseapp.com",
+     databaseURL: "https://dropbox-clone-13ef0-default-rtdb.firebaseio.com",
+     projectId: "dropbox-clone-13ef0",
+     storageBucket: "dropbox-clone-13ef0.appspot.com",
+     messagingSenderId: "559956481192",
+     appId: "1:559956481192:web:21cacf79f060c591ee5e9d"
+ };
